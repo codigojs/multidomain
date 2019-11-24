8b58fd170dc7b579f6f53d2f6522ebc11a06e75c
Roylan Suarez <roylan@bandbcuba.com>
2019-11-23 21:09:16 -0500

    commit inicial

---
+++ src/layouts/MyLayout.vue
@@ -0,0 +1,65 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          icon="menu"
+          aria-label="Menu"
+        />
+
+        <q-toolbar-title>
+          Mi sitio web multi dominio
+        </q-toolbar-title>
+
+        <div></div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-2"
+    >
+      <q-list>
+        <q-item-label header>{{ $t('menu') }}</q-item-label>
+        <q-item clickable tag="a" target="_blank">
+          <q-item-section avatar>
+            <q-icon name="home" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>{{ $t('inicio') }}</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable tag="a" target="_blank">
+          <q-item-section avatar>
+            <q-icon name="code" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>{{ $t('acerca') }}</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'MyLayout',
+
+  data () {
+    return {
+      leftDrawerOpen: false
+    }
+  }
+}
+</script>
